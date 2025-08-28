2bc3696404fcedb39772d8e6aa01dd55f07d53ca
jouyai <iaarybudiarto@gmail.com>
2025-08-29 00:24:51 +0700

    feat: Add error page component with suspense loading

---
+++ app/payment/error/page.jsx
@@ -0,0 +1,10 @@
+import React, { Suspense } from 'react';
+import ErrorContent from './ErrorContent';
+
+export default function ErrorPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ErrorContent />
+    </Suspense>
+  );
+}
